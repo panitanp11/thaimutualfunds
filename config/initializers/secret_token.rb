f5186510918e163d7434e8baf9f9ec666b493349
Panitan <panitan@ubuntu.(none)>
2013-07-15 22:57:07 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thaimutualfunds::Application.config.secret_token = '779e735887fd8c7a1ebe150a38a6f40156ae636dbb20ceb304534361f2672024084158412bb4af3e65ee380de6820cff8063411116e160bb3cdf9264865aafe4'
